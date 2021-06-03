7cdd18b675cfb57cce643b735ad8e49c104c6035
jakupEmini <jakup.emini.5@gmail.com>
2021-06-03 11:30:26 +0200

    Frist draft

---
+++ Minesweeper/Assets/BoardScript.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardScript : MonoBehaviour {
+    // Start is called before the first frame update
+    public SingleBlockScript singleBlock;
+    public int height;
+    public int width;
+    public int bombs;
+    public SingleBlockScript[,] myBoard;
+    public bool isFirstClick = true;
+    public int numberOfOppenedBlocks = 0;
+
+    public GameObject panel;
+    public Text endText;
+    public bool isGameOver = false;
+    void Start() {
+        myBoard = new SingleBlockScript[width, height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                myBoard[i, j] = Instantiate(singleBlock, new Vector3(this.transform.position.x + i, this.transform.position.y, this.transform.position.z - j), Quaternion.identity);
+                myBoard[i, j].i = i;
+                myBoard[i, j].j = j;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonUp(0) && !isGameOver) {
+            SingleBlockScript rig = getRayCastHit();
+            if(rig!=null) {
+                if (isFirstClick) {
+                    rig.isFirstCube = true;
+                    firstClickRandomize();
+                    isFirstClick = false;
+                }
+                rig.openCube();
+            }
+
+        }
+        if(Input.GetMouseButtonUp(1) && !isGameOver) {
+            SingleBlockScript rig = getRayCastHit();
+            if(rig!=null) {
+                rig.setFlag();
+            }
+        }
+    }
+
+    public SingleBlockScript getRayCastHit() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            SingleBlockScript rig = hit.collider.GetComponent<SingleBlockScript>();
+
+            return rig;
+        }
+        return null;
+
+    }
+
+    public void firstClickRandomize() {
+        for (int tempCounter = 0; tempCounter < bombs; tempCounter++) {
+            int i = Random.Range(0, width - 1);
+            int j = Random.Range(0, height - 1);
+
+            if (myBoard[i, j].hasBomb || myBoard[i, j].isFirstCube) {
+                tempCounter--;
+            }
+            else {
+                myBoard[i, j].hasBomb = true;
+            }
+
+        }
+
+        for (int i = 0; i < width; i++) {
+
+            for (int j = 0; j < height; j++) {
+                myBoard[i, j].calculateNumberOfBombs();
+            }
+        }
+    }
+}
---
+++ Minesweeper/Assets/SingleBlockScript.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SingleBlockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cube;
+    
+    public int i;
+    public int j;
+
+    public int numberOfBombsAround = 0;
+    public int numberOfFlagsAround = 0;
+    public bool hasBomb = false;
+    public bool hasBeenOpened = false;
+    public bool isFirstCube = false;
+    public Text bombsText;
+
+    private BoardScript myBoardScript;
+    private bool isFlagged = false;
+
+
+    
+
+    
+    void Start()
+    {
+        
+        myBoardScript = FindObjectOfType<BoardScript>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setFlag() {
+        isFlagged = !isFlagged;
+        if(isFlagged) {
+            GetComponentInChildren<Renderer>().material.color = Color.red;
+        } else {
+            GetComponentInChildren<Renderer>().material.color = Color.gray;
+
+        }
+    }
+    public void openCube() {
+        if(hasBeenOpened) {
+            
+            calculateNumberOfBombs();
+            if(numberOfBombsAround == numberOfFlagsAround) {
+                Debug.Log("should clear here");
+                // clear when clicking already oppened block FEATURE
+            }
+        } else {
+            myBoardScript.numberOfOppenedBlocks++;
+
+        }
+        hasBeenOpened = true;
+        if (hasBomb) {
+            Renderer temp = GetComponentInChildren<Renderer>();
+            temp.material.color = Color.black;
+            // end game panel
+
+            myBoardScript.isGameOver = true;
+            myBoardScript.panel.SetActive(true);
+            myBoardScript.endText.text = "Game Over!";
+
+
+        }
+        else if (!hasBomb) {
+            if (this.numberOfBombsAround == 0)
+                floodFill();
+            Destroy(cube);
+        }
+
+        if(myBoardScript.numberOfOppenedBlocks+myBoardScript.bombs == myBoardScript.width*myBoardScript.height) {
+            // Game won panel
+
+            myBoardScript.isGameOver = true;
+            myBoardScript.panel.SetActive(true);
+            myBoardScript.endText.text = "You have Won!";
+        }
+        
+    }
+    public void calculateNumberOfBombs() {
+        int bombcounter = 0;
+        for (int xoff = -1; xoff <= 1; xoff++) {
+            for (int yoff = -1; yoff <= 1; yoff++) {
+                int x = this.i + xoff;
+                int y = this.j + yoff;
+                if (x > -1 && x < myBoardScript.width && y > -1 && y < myBoardScript.height) {
+                    if (myBoardScript.myBoard[x, y].hasBomb)
+                        bombcounter++;
+                    if (myBoardScript.myBoard[x, y].isFlagged) {
+                        numberOfFlagsAround++;
+                    }
+                }
+            }
+        }
+        this.numberOfBombsAround = bombcounter;
+        updateText();
+    }
+    public void updateText() {
+        if(numberOfBombsAround !=0)
+            bombsText.text = numberOfBombsAround.ToString();
+    }
+    private void floodFill() {
+        for(int xoff=-1; xoff<=1; xoff++) {
+            for(int yoff=-1; yoff<=1; yoff++) {
+                int x = this.i + xoff;
+                int y = this.j + yoff;
+                if(x>-1 && x<myBoardScript.width && y>-1 && y<myBoardScript.height) {
+                    SingleBlockScript neighbour = myBoardScript.myBoard[x,y];
+                    if(!neighbour.hasBomb) {
+                        if(!neighbour.hasBeenOpened)
+                            neighbour.openCube();
+                    }
+                }
+            }
+        }
+    }
+
+    
+}
